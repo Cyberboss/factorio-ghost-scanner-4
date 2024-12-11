82cc2f717ee1a352ebe407ffea0e93ee2d073ea7
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-12-10 20:20:02 -0500

    WIP

---
+++ src/prototypes/entity.ts
@@ -0,0 +1,35 @@
+import { Sprite4Way } from "factorio:prototype";
+import { table, by_pixel } from "util";
+
+const layers = {
+    layers: [
+        {
+            scale: 0.5,
+            filename: "__GhostScanner4__/graphics/entity/hr-ghost-scanner.png",
+            width: 114,
+            height: 102,
+            frame_count: 1,
+            shift: by_pixel(0, 5)
+        },
+        {
+            scale: 0.5,
+            filename: "__base__/graphics/entity/combinator/constant-combinator-shadow.png",
+            width: 98,
+            height: 66,
+            frame_count: 1,
+            shift: by_pixel(8.5, 5.5),
+            draw_as_shadow: true
+        }
+    ]
+};
+declare function make_4way_animation_from_spritesheet(this: void, input: any): Sprite4Way;
+const sprites = make_4way_animation_from_spritesheet(layers);
+
+const scanner = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])!;
+scanner.name = "ghost-scanner";
+scanner.icon = "__GhostScanner4__/graphics/icons/ghost-scanner.png";
+scanner.icon_size = 32;
+scanner.minable!.result = "ghost-scanner";
+scanner.sprites = sprites;
+
+data.extend([scanner]);
